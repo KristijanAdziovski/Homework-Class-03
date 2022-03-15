1bfcf15f1513bf8c75e376582f7c1386a64403a2
KristijanAdziovski <kristijanadziovskiadzi@gmail.com>
2022-03-16 00:11:15 +0100

    Add project files.

---
+++ Homework Class 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            int i;
+            Console.Write("Input 6 elements in the array :\n");
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Write("\n");
+
+            int sum = 0;
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (arr[j] % 2 == 0)
+                {
+                    sum += arr[j];
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+
+
+}
---
+++ StudentGroup/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] studentGOne = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsGTwo = { "Zoki", "Mile", "Brus", "Petko", "Vele" };
+
+            Console.WriteLine("Pleas input 1 or 2 to print students list");
+            string list = Console.ReadLine();
+            int numberList = int.Parse(list);
+            switch (numberList)
+            {
+                case 1:
+
+                    Console.WriteLine("The Students in G1 are: /n");
+
+                    for ( int i = 0; i <studentGOne.Length; i++)
+                    {
+                        Console.Write("{0} \n ", studentGOne[i]);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("The Students in G1 are: \n");
+
+                    for ( int j = 0; j < studentsGTwo.Length; j++)
+                    {
+                       
+                        Console.Write("{0} \n", studentsGTwo[j]);
+                    }
+                    break ;
+
+                default:
+                    Console.WriteLine("You don't input 1 or 2 ");
+                    break ;
+            }
+        }
+    }
+}
